e10cfce400d66a215a76f88b3b7e79b3e5e10ca9
ceejaytalinio <christianjoy.talinio@gmail.com>
2021-11-20 12:19:26 +0800

    basic functionalities more to add

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.R1A = new System.Windows.Forms.Button();
+            this.R1B = new System.Windows.Forms.Button();
+            this.R1C = new System.Windows.Forms.Button();
+            this.R2A = new System.Windows.Forms.Button();
+            this.R2B = new System.Windows.Forms.Button();
+            this.R2C = new System.Windows.Forms.Button();
+            this.R3A = new System.Windows.Forms.Button();
+            this.R3B = new System.Windows.Forms.Button();
+            this.R3C = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // R1A
+            // 
+            this.R1A.BackColor = System.Drawing.Color.Honeydew;
+            this.R1A.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R1A.ForeColor = System.Drawing.Color.Black;
+            this.R1A.Location = new System.Drawing.Point(82, 77);
+            this.R1A.Name = "R1A";
+            this.R1A.Size = new System.Drawing.Size(172, 153);
+            this.R1A.TabIndex = 1;
+            this.R1A.UseVisualStyleBackColor = false;
+            this.R1A.Click += new System.EventHandler(this.tile_click);
+            // 
+            // R1B
+            // 
+            this.R1B.BackColor = System.Drawing.Color.Honeydew;
+            this.R1B.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R1B.ForeColor = System.Drawing.Color.Black;
+            this.R1B.Location = new System.Drawing.Point(261, 77);
+            this.R1B.Name = "R1B";
+            this.R1B.Size = new System.Drawing.Size(172, 153);
+            this.R1B.TabIndex = 2;
+            this.R1B.UseVisualStyleBackColor = false;
+            this.R1B.Click += new System.EventHandler(this.tile_click);
+            // 
+            // R1C
+            // 
+            this.R1C.BackColor = System.Drawing.Color.Honeydew;
+            this.R1C.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R1C.ForeColor = System.Drawing.Color.Black;
+            this.R1C.Location = new System.Drawing.Point(440, 77);
+            this.R1C.Name = "R1C";
+            this.R1C.Size = new System.Drawing.Size(172, 153);
+            this.R1C.TabIndex = 3;
+            this.R1C.UseVisualStyleBackColor = false;
+            this.R1C.Click += new System.EventHandler(this.tile_click);
+            // 
+            // R2A
+            // 
+            this.R2A.BackColor = System.Drawing.Color.Honeydew;
+            this.R2A.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R2A.ForeColor = System.Drawing.Color.Black;
+            this.R2A.Location = new System.Drawing.Point(82, 236);
+            this.R2A.Name = "R2A";
+            this.R2A.Size = new System.Drawing.Size(172, 153);
+            this.R2A.TabIndex = 4;
+            this.R2A.UseVisualStyleBackColor = false;
+            this.R2A.Click += new System.EventHandler(this.tile_click);
+            // 
+            // R2B
+            // 
+            this.R2B.BackColor = System.Drawing.Color.Honeydew;
+            this.R2B.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R2B.ForeColor = System.Drawing.Color.Black;
+            this.R2B.Location = new System.Drawing.Point(261, 236);
+            this.R2B.Name = "R2B";
+            this.R2B.Size = new System.Drawing.Size(172, 153);
+            this.R2B.TabIndex = 5;
+            this.R2B.UseVisualStyleBackColor = false;
+            this.R2B.Click += new System.EventHandler(this.tile_click);
+            // 
+            // R2C
+            // 
+            this.R2C.BackColor = System.Drawing.Color.Honeydew;
+            this.R2C.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R2C.ForeColor = System.Drawing.Color.Black;
+            this.R2C.Location = new System.Drawing.Point(440, 236);
+            this.R2C.Name = "R2C";
+            this.R2C.Size = new System.Drawing.Size(172, 153);
+            this.R2C.TabIndex = 6;
+            this.R2C.UseVisualStyleBackColor = false;
+            this.R2C.Click += new System.EventHandler(this.tile_click);
+            // 
+            // R3A
+            // 
+            this.R3A.BackColor = System.Drawing.Color.Honeydew;
+            this.R3A.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R3A.ForeColor = System.Drawing.Color.Black;
+            this.R3A.Location = new System.Drawing.Point(82, 395);
+            this.R3A.Name = "R3A";
+            this.R3A.Size = new System.Drawing.Size(172, 153);
+            this.R3A.TabIndex = 7;
+            this.R3A.UseVisualStyleBackColor = false;
+            this.R3A.Click += new System.EventHandler(this.tile_click);
+            // 
+            // R3B
+            // 
+            this.R3B.BackColor = System.Drawing.Color.Honeydew;
+            this.R3B.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R3B.ForeColor = System.Drawing.Color.Black;
+            this.R3B.Location = new System.Drawing.Point(261, 395);
+            this.R3B.Name = "R3B";
+            this.R3B.Size = new System.Drawing.Size(172, 153);
+            this.R3B.TabIndex = 8;
+            this.R3B.UseVisualStyleBackColor = false;
+            this.R3B.Click += new System.EventHandler(this.tile_click);
+            // 
+            // R3C
+            // 
+            this.R3C.BackColor = System.Drawing.Color.Honeydew;
+            this.R3C.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.R3C.ForeColor = System.Drawing.Color.Black;
+            this.R3C.Location = new System.Drawing.Point(440, 395);
+            this.R3C.Name = "R3C";
+            this.R3C.Size = new System.Drawing.Size(172, 153);
+            this.R3C.TabIndex = 9;
+            this.R3C.UseVisualStyleBackColor = false;
+            this.R3C.Click += new System.EventHandler(this.tile_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(709, 607);
+            this.Controls.Add(this.R3C);
+            this.Controls.Add(this.R3B);
+            this.Controls.Add(this.R3A);
+            this.Controls.Add(this.R2C);
+            this.Controls.Add(this.R2B);
+            this.Controls.Add(this.R2A);
+            this.Controls.Add(this.R1C);
+            this.Controls.Add(this.R1B);
+            this.Controls.Add(this.R1A);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button R1A;
+        private System.Windows.Forms.Button R1B;
+        private System.Windows.Forms.Button R1C;
+        private System.Windows.Forms.Button R2A;
+        private System.Windows.Forms.Button R2B;
+        private System.Windows.Forms.Button R2C;
+        private System.Windows.Forms.Button R3A;
+        private System.Windows.Forms.Button R3B;
+        private System.Windows.Forms.Button R3C;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true;
+        int turn_count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void tile_click(object sender, EventArgs e)
+        {
+            Button x = (Button)sender;
+            if (turn)
+                x.Text = "X";
+            else
+                x.Text = "O";
+
+            turn = !turn;
+            x.Enabled = false;
+            winningCombo();
+        }
+        private void winningCombo()
+        {
+            bool winner = false;
+            turn_count++;
+
+            //WIN CONDITIONS
+            if ((R1A.Text == R2A.Text) && (R2A.Text == R3A.Text) && (R1A.Enabled == false))
+                winner = true;
+            if ((R1B.Text == R2B.Text) && (R2B.Text == R3B.Text) && (R1B.Enabled == false))
+                winner = true;
+            if ((R1C.Text == R2C.Text) && (R2C.Text == R3C.Text) && (R1C.Enabled == false))
+                winner = true;
+            if ((R1A.Text == R1B.Text) && (R1B.Text == R1C.Text) && (R1A.Enabled == false))
+                winner = true;
+            if ((R2A.Text == R2B.Text) && (R2B.Text == R2C.Text) && (R2A.Enabled == false))
+                winner = true;
+            if ((R3A.Text == R3B.Text) && (R3B.Text == R3C.Text) && (R3A.Enabled == false))
+                winner = true;
+            if ((R1A.Text == R2B.Text) && (R2B.Text == R3C.Text) && (R1A.Enabled == false))
+                winner = true;
+            if ((R1C.Text == R2B.Text) && (R2B.Text == R3A.Text) && (R3A.Enabled == false))
+                winner = true;
+
+            if (winner)
+            {
+                String player = "";
+                if (!turn)
+                    player = "X";
+                else
+                    player = "O";
+
+                MessageBox.Show(player + " Is The Victor!");
+                Application.Exit();
+            }
+            if (turn_count == 9)
+            {
+                MessageBox.Show("Sorry! No Winners Today");
+                Application.Exit();
+            }
+        }
+    }
+}
